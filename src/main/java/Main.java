7a933fa33dd641f3673ca5ce34d57ecc8991c878
Oliverden3 <oliversjensen@hotmail.com>
2023-01-15 21:12:02 +0100

    Kinda works even more now

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,29 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import entities.Role;
+import entities.User;
+import facades.UserFacade;
+import utils.EMF_Creator;
+
+import javax.management.relation.RoleList;
+import javax.persistence.EntityManagerFactory;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello");
+
+        EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
+        UserFacade FACADE =  UserFacade.getUserFacade(EMF);
+
+        Role role = new Role("admin");
+        List RoleList = new ArrayList<>();
+        RoleList.add(role);
+
+        User user = new User("shawty","test123",RoleList);
+
+        FACADE.createUser(user);
     }
 }
