07fad7e3a21306c75043cb1419733574edfa4aea
Oliverden3 <oliversjensen@hotmail.com>
2023-01-16 22:37:38 +0100

    Fixed entitites a bit

--- src/main/java/entities/Trip.java
+++ src/main/java/entities/Trip.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Objects;
 
 @Entity
-@Table(name = "Table")
+@Table(name = "Trip")
 @NamedQuery(name = "Trip.deleteAllRows", query = "DELETE from Trip")
 public class Trip {
     @Id()
